f53d0b13ab097cc20784bcfdc924c1056826d43a
jiwon <parkjiwon@Park-Jiwon.local>
2022-03-31 22:32:44 +0900

    test

--- babkaotalk/settings.py
+++ babkaotalk/settings.py
@@ -114,9 +114,9 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-CORS_ORIGIN_WHITELIST = ['http://127.0.0.1:4000' ,'http://localhost:4000'] 
-CORS_ALLOW_CREDENTIALS = True
-
+# CORS_ORIGIN_WHITELIST = ['http://127.0.0.1:4000' ,'http://localhost:4000'] 
+# CORS_ALLOW_CREDENTIALS = True
+CORS_ORIGIN_ALLOW_ALL = True
 
 
 # Internationalization
