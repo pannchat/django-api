81ccac9d1aea44d21f9b6ff9540e8e6a260bf1fa
jiwon <parkjiwon@Park-Jiwon.local>
2022-05-01 00:33:44 +0900

    test

--- api/views.py
+++ api/views.py
@@ -153,8 +153,10 @@ def index(request):
                     _ret['template']['outputs'][1]['carousel']['items'].append(tmp)
                 print(json_data[요일][발화][식당]['메뉴'],json_data[요일][발화][식당]['후식'],json_data[요일][발화][식당]['식수'])
                 # print(_ret['template']['outputs'][1]['carousel']['items'])
+        print((_ret['template']['outputs'][1]['carousel']['items']))
         if len(_ret['template']['outputs'][1]['carousel']['items']) == 0:
             _ret['template']['outputs'][0]['simpleText']['text'] = "응답 내용이 없습니다. 문제가 발생했거나 식사가 제공되지 않나봐요."
+            del _ret['template']['outputs'][1]
         _ret = json.dumps(_ret, ensure_ascii=False)
 
         return HttpResponse(_ret)
