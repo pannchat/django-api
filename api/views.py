ac689b4590ead92a306653d56bfceebcabca7ffd
jiwon <parkjiwon@Park-Jiwon.local>
2022-05-01 00:04:14 +0900

    폴백

--- api/views.py
+++ api/views.py
@@ -151,8 +151,8 @@ def index(request):
                 }
                 if len(json_data[요일][발화][식당]['메뉴'])>0 or len(json_data[요일][발화][식당]['후식'])>0 or len(json_data[요일][발화][식당]['식수']) > 0:
                     _ret['template']['outputs'][1]['carousel']['items'].append(tmp)
-                elif len(json_data[요일][발화][식당]['메뉴'])==0 and len(json_data[요일][발화][식당]['후식'])==0 and len(json_data[요일][발화][식당]['식수']) == 0 :
-                    _ret['template']['outputs'][0]['simpleText']['text'] = "응답 내용이 없습니다. 문제가 발생했거나 식사가 제공되지 않나봐요."
+        if len(_ret['template']['outputs'][1]['carousel']['items']) == 0:
+            _ret['template']['outputs'][0]['simpleText']['text'] = "응답 내용이 없습니다. 문제가 발생했거나 식사가 제공되지 않나봐요."
         _ret = json.dumps(_ret, ensure_ascii=False)
 
         return HttpResponse(_ret)
