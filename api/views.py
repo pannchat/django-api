93b06e730363693c800b86ffbfc96af3682915f3
박지원 <parkjiwon@bagjiwon-ui-MacBook-Pro.local>
2022-06-29 21:28:48 +0900

    bread

--- api/views.py
+++ api/views.py
@@ -205,8 +205,7 @@ def bread(request):
                         "itemCard": {
                             "title": "",
                             "thumbnail": {
-                                # "imageUrl": "https://babkaotalk.herokuapp.com/static/bread/" + str(breadIdx+1+2) + '.jpg',
-                                "imageUrl": "https://babkaotalk.herokuapp.com" + arr['img'],
+                                "imageUrl": "https://babkaotalk.herokuapp.com/static/bread/" + str(breadIdx+1+3) + '.jpg',
                                 "width": 800,
                                 "height": 800
                             },
